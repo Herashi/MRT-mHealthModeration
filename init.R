caf24985e3f02570918d80fd9da74f3872e506b3
Herashi <shijr96@gmail.com>
2020-05-26 18:14:55 -0400

    first commit

---
+++ init.R
@@ -0,0 +1,9 @@
+library("geepack")
+library("zoo")
+
+source("xgeepack.R")
+source("xzoo.R")
+
+# system("R CMD SHLIB rsnmm.c")
+# dyn.load(if (Sys.info()["sysname"] == "Windows") "rsnmm.dll" else "rsnmm.so")
+source("rsnmm.R")
