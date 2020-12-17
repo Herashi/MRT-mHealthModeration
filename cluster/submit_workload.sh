16a4bb9d0c9d5c3e908afb65f45e54bdce474782
Hera SHI <shijr96@gmail.com>
2020-12-17 13:20:45 -0500

    delete

--- cluster/submit_workload.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-part1tmax30=$(sbatch --job-name=tmax30 --export=ALL,tmax=30 part1.slurm | awk '{print $NF}')
-part1tmax50=$(sbatch --job-name=tmax50 --export=ALL,tmax=50 part1.slurm | awk '{print $NF}')
-sbatch --dependency=afterok:$part1tmax30:$part1tmax50 part2.slurm
